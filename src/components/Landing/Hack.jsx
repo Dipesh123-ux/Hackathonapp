892a0f70770b44b33f0a1e669aa86d1dd455b070
dipesh <jaswanidipesh8@gmail.com>
2022-10-09 21:18:06 +0530

    image added

--- src/components/Landing/Hack.jsx
+++ src/components/Landing/Hack.jsx
@@ -5,7 +5,7 @@ const Hack = ({hack}) => {
   return (
  
      <div className="h-96  mt-5 mb-5 rounded bg-white">
-        <div style={{backgroundImage : `url("http://localhost:8080/api/photo/${hack._id}")`,backgroundSize:"cover"}} className="h-2/5 bg-white"></div>
+        <div style={{backgroundImage : `url("https://hackathondphi.herokuapp.com/api/photo/${hack._id}")`,backgroundSize:"cover"}} className="h-2/5 bg-white"></div>
         <div className="text-center h-3/5">
             <Timer id={hack._id} title={hack.title} start={hack.start} end={hack.end} />
         </div>
