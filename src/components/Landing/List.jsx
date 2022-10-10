9654eb1c242b69aad8a1e59a5f9469d73859721b
dipesh <jaswanidipesh8@gmail.com>
2022-10-10 12:33:37 +0530

    loader-added

--- src/components/Landing/List.jsx
+++ src/components/Landing/List.jsx
@@ -7,25 +7,27 @@ import FormControl from '@mui/material/FormControl';
 import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
+import { RotatingLines } from 'react-loader-spinner'
+
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
     },
-  },
 };
 
 const names = [
-  'upcoming',
-  'active',
-  'past',
-  'easy',
-  'medium',
-  'hard'
+    'upcoming',
+    'active',
+    'past',
+    'easy',
+    'medium',
+    'hard'
 ];
 
 
@@ -36,20 +38,20 @@ const List = () => {
     const [selected, setSelected] = React.useState([]);
 
     const handleChange2 = (event) => {
-      const {
-        target: { value },
-      } = event;
-      setSelected(
-        // On autofill we get a stringified value.
-        typeof value === 'string' ? value.split(',') : value,
-      );
+        const {
+            target: { value },
+        } = event;
+        setSelected(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
     };
 
     const handleChange = (e) => {
         setQuery(e.target.value);
     }
 
-console.log(selected);
+    console.log(selected);
     useEffect(() => {
         fetch('https://hackathondphi.herokuapp.com/api/hackathons', {
             method: 'GET',
@@ -73,39 +75,39 @@ console.log(selected);
             )
         }
 
-        if(selected.length > 0 && updatedHack.length > 0){
-            selected.forEach((item)=>{
-                if(item === "easy"){
-                    updatedHack = updatedHack.filter((h)=>{
+        if (selected.length > 0 && updatedHack.length > 0) {
+            selected.forEach((item) => {
+                if (item === "easy") {
+                    updatedHack = updatedHack.filter((h) => {
                         return h.level.toLowerCase() === "easy";
                     })
                 }
-                else if(item === "medium"){
-                    updatedHack = updatedHack.filter((h)=>{
+                if (item === "medium") {
+                    updatedHack = updatedHack.filter((h) => {
                         return h.level.toLowerCase() === "medium";
                     })
                 }
-                else if(item === "hard"){
-                    updatedHack = updatedHack.filter((h)=>{
+                if (item === "hard") {
+                    updatedHack = updatedHack.filter((h) => {
                         return h.level.toLowerCase() === "hard";
                     })
                 }
-                if(item === "upcoming"){
+                if (item === "upcoming") {
                     let c = new Date().getTime();
-                    updatedHack = updatedHack.filter((h)=>{
-                        return  parseInt(h.start) > c;
+                    updatedHack = updatedHack.filter((h) => {
+                        return parseInt(h.start) > c;
                     })
                 }
-                if(item === "active"){
+                if (item === "active") {
                     let c = new Date().getTime();
-                    updatedHack = updatedHack.filter((h)=>{
-                        return (parseInt(h.start) < c) && (parseInt(h.end) > c);
+                    updatedHack = updatedHack.filter((h) => {
+                        return ((parseInt(h.start) < c) && (parseInt(h.end) > c));
                     })
                 }
-                if(item === "past"){
+                if (item === "past") {
                     let c = new Date().getTime();
-                    updatedHack = updatedHack.filter((h)=>{
-                        return (parseInt(h.start) < c) && (parseInt(h.end) < c);
+                    updatedHack = updatedHack.filter((h) => {
+                        return ((parseInt(h.start) < c) && (parseInt(h.end) < c));
                     })
                 }
             })
@@ -115,49 +117,78 @@ console.log(selected);
 
 
 
-    }, [hackathons, query,selected])
-    return (
-        <div className="">
-            <div className="flex justify-center flex-col h-40 bg-secondary">
-                <div className="mt-4 text-white text-xl font-bold ml-auto mr-auto mb-4">Explore Challenges</div>
-                <div className="flex">
-                  <div class="w-full text-center mb-3 md:mb-0">
-                    <input class="appearance-none m-auto block w-2/4 bg-gray-200 text-gray-700 border rounded py-1 px-3 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Search" onChange={handleChange} />
-                </div>
-                <div className="mr-40 mb-8" >
-      <FormControl style={{border:"1px solid white"}} sx={{ m: 1, width: 200 }}>
-        <InputLabel style={{color:"white"}} id="demo-multiple-checkbox-label">Filter</InputLabel>
-        <Select
-          labelId="demo-multiple-checkbox-label"
-          id="demo-multiple-checkbox"
-          multiple
-          value={selected}
-          onChange={handleChange2}
-          input={<OutlinedInput label="Tag" />}
-          renderValue={(se) => se.join(', ')}
-          MenuProps={MenuProps}
-        >
-          {names.map((name) => (
-            <MenuItem key={name} value={name}>
-              <Checkbox checked={selected.indexOf(name) > -1} />
-              <ListItemText primary={name} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
-                </div>
-            </div>
-            <div className="h-max bg-primary">
-                <div className="ml-40 mr-40 grid md:grid-cols-3 gap-10">
-                    {Final && Final.map((item) => (
-                        <Hack hack={item} />
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
-}
+    }, [hackathons, query, selected])
 
 
-export default List
+    const showList = () => {
+
+
+        if (Final.length == 0) {
+
+            return <div className="bg-primary h-48 flex justify-center">
+                <RotatingLines
+
+                    strokeColor="grey"
+                    strokeWidth="3"
+                    animationDuration="0.75"
+                    width="80"
+                    visible={true}
+                />
+            </div>
+        }
+
+        else {
+
+            return <div className="ml-40 mr-40 grid md:grid-cols-3 gap-10">
+                {Final.map((item) => (
+                    <Hack hack={item} />
+                ))
+                }
+            </div>
+        }
+
+    }
+
+   return (
+            <div className="">
+                <div className="flex justify-center flex-col h-40 bg-secondary">
+                    <div className="mt-4 text-white text-xl font-bold ml-auto mr-auto mb-4">Explore Challenges</div>
+                    <div className="flex">
+                        <div class="w-full text-center mb-3 md:mb-0">
+                            <input class="appearance-none m-auto block w-2/4 bg-gray-200 text-gray-700 border rounded py-1 px-3 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Search" onChange={handleChange} />
+                        </div>
+                        <div className="mr-40 mb-8" >
+                            <FormControl style={{ border: "1px solid white" }} sx={{ m: 1, width: 200 }}>
+                                <InputLabel style={{ color: "white" }} id="demo-multiple-checkbox-label">Filter</InputLabel>
+                                <Select
+                                    labelId="demo-multiple-checkbox-label"
+                                    id="demo-multiple-checkbox"
+                                    multiple
+                                    value={selected}
+                                    onChange={handleChange2}
+                                    input={<OutlinedInput label="Tag" />}
+                                    renderValue={(se) => se.join(', ')}
+                                    MenuProps={MenuProps}
+                                >
+                                    {names.map((name) => (
+                                        <MenuItem key={name} value={name}>
+                                            <Checkbox checked={selected.indexOf(name) > -1} />
+                                            <ListItemText primary={name} />
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </div>
+                    </div>
+                </div>
+                <div className="h-max bg-primary">
+
+                    {showList()}
+
+                </div>
+            </div>
+        )
+    }
+
+
+    export default List
